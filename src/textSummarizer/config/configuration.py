af9ea5d7217634be819b320fd1bf065973433168
Mohit Kumar <mohitcnf95@gmail.com>
2023-09-26 17:49:43 +0530

    Update

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,8 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from src.textSummarizer.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
-from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig,
+                                   DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -46,3 +47,19 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+
+
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
